bc974694a931617af9f3fa184e5baf4efbca2c70
DIVESH KUMAR <diveshkumar8812@gmail.com>
2021-06-23 14:10:40 +0530

    Time: 32 ms (20.66%), Space: 13.2 MB (5.59%) - LeetHub

--- longest-common-subsequence/longest-common-subsequence.cpp
+++ longest-common-subsequence/longest-common-subsequence.cpp
@@ -7,21 +7,21 @@ public:
         if(i==0 || j==0)
             return 0;
         
-        else if(dp[i-1][j-1]!=-1)
-            return dp[i-1][j-1];
+        else if(dp[i][j]!=-1)
+            return dp[i][j];
         
         else if(s[i-1]==r[j-1])
-            return dp[i-1][j-1]=1+lcs(s,r,dp,i-1,j-1);
+            return dp[i][j]=1+lcs(s,r,dp,i-1,j-1);
 
-        return dp[i-1][j-1]=max(lcs(s,r,dp,i-1,j),lcs(s,r,dp,i,j-1));
+        return dp[i][j]=max(lcs(s,r,dp,i-1,j),lcs(s,r,dp,i,j-1));
     }
     
     int longestCommonSubsequence(string s, string r) {
         int i=s.length();
         int j=r.length();
-        if(i<j)
-            return longestCommonSubsequence(r,s);
-        vector<vector<int>>dp(s.length()+1,vector<int>(s.length()+1,-1));
+        // if(i<j)
+        //     return longestCommonSubsequence(r,s);
+        vector<vector<int>>dp(s.length()+1,vector<int>(r.length()+1,-1));
         return lcs(s,r,dp,i,j);
     }
 };
