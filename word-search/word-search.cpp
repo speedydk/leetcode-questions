6d91372a497613dcebcac0aad9f1551312ec39ca
DIVESH KUMAR <diveshkumar8812@gmail.com>
2021-08-06 00:59:45 +0530

    Time: 512 ms (25.99%), Space: 7.4 MB (18.73%) - LeetHub

--- word-search/word-search.cpp
+++ word-search/word-search.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     
-    bool dfs(vector<vector<char>>&board, string word, int index, int i, int j, int row, int col, vector<vector<bool>>&visited)
+    bool dfs(vector<vector<char>>&board, string word, int index, int i, int j, int row, int col)
     {
     
-        if(index == word.size())
-        return true;
         
-        if(i<0 || i==row || j<0 || j==col || visited[i][j]==true)
+        if(i<0 || i==row || j<0 || j==col || board[i][j]!=word[index] || index>=word.length())
             return false;
         
-        visited[i][j]=true;
+        if(index == word.size()-1)
+        return true;
+    
+        char ch= board[i][j];
+        board[i][j]='0';
         
-        if(board[i][j] == word[index])
-        {
-            if(dfs(board, word, index + 1, i+1, j, row, col, visited) || dfs(board, word, index + 1, i, j+1, row, col, visited) || dfs(board, word, index + 1, i-1, j, row, col, visited) || dfs(board, word, index + 1, i, j-1, row, col, visited))
+        if(dfs(board, word, index + 1, i+1, j, row, col) || dfs(board, word, index + 1, i, j+1, row, col) || dfs(board, word, index + 1, i-1, j, row, col) || dfs(board, word, index + 1, i, j-1, row, col))
                 return true;
-        } 
         
-        visited[i][j]=false;
+        board[i][j]=ch;
         return false;
     }
     
     bool exist(vector<vector<char>>& board, string word) {
         int row= board.size(), col= board[0].size();
-        vector<vector<bool>>visited(row, vector<bool>(col,false));
         int index=0;
         
         for(int i=0; i<row; i++)
         {
             for(int j=0; j<col; j++)
             {
-                    if(dfs(board, word, index, i, j, row, col, visited))
+                    if(dfs(board, word, index, i, j, row, col))
                         return true;
             }
         }
