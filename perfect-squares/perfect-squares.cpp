5f0d1e2593fc755fb131c3cbd92fa5452e1ef254
DIVESH KUMAR <diveshkumar8812@gmail.com>
2021-08-09 12:24:44 -0700

    Time: 100 ms (66.21%), Space: 9 MB (55.74%) - LeetHub

---
+++ perfect-squares/perfect-squares.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        
+        vector<int>dp(n+1,0);
+        dp[1]= 1;
+        
+        for(int i=2; i<=n; i++)
+        {
+            int ans= INT_MAX;
+            for(int j=1; j<=sqrt(i); j++)
+            {
+                int k= j*j;
+                ans= min(ans, 1 + dp[i-k]);
+            }
+            
+            dp[i]= ans;
+        }
+        
+        
+        return dp[n];
+    }
+};
