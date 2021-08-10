94603821eb5da73f6670f12e31bce343f9f7fe7c
DIVESH KUMAR <diveshkumar8812@gmail.com>
2021-08-10 01:32:42 -0700

    Time: 8 ms (33.73%), Space: 8.3 MB (47.97%) - LeetHub

--- last-stone-weight-ii/last-stone-weight-ii.cpp
+++ last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
     int lastStoneWeightII(vector<int>& arr) {
+        
+        int size= arr.size();
         int sum=0;
-        int n= arr.size();
-	    for(int i=0; i<n; i++)
-	    sum+= arr[i];
-	    
-	    vector<vector<bool>>dp(n+1,vector<bool>(sum+1,false));
-	    int mini_diff= INT_MAX;
-	    
-	    for(int i=0; i<n+1; i++)
-	    {
-	        for(int j=0; j<sum+1; j++)
-	        {
-	            if(j==0)
-	            {
-	                dp[i][j] = true;
-	                continue;
-	            }
-	            
-	            if(i==0)
-	            continue;
-	            
-	            if(j>= arr[i-1])
-	            dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
-	            
-	            else
-	            dp[i][j] = dp[i-1][j];
-	            
-	            if(dp[i][j] == true)
-	            {
-	                int set1 = j;
-	                int set2 = sum - j;
-	                mini_diff = min(mini_diff, abs(set1-set2));;
-	            }
-	        }
-	    }
-	    
-	    return mini_diff;
+        
+        for(auto it: arr)
+            sum+= it;
+        
+        vector<vector<bool>>dp(size+1,vector<bool>(sum/2+1,false));
+        dp[0][0]= true;
+        
+        for(int i=1; i<=size; i++)
+        {
+            for(int j=0; j<=sum/2; j++)
+            {
+                if(j==0)
+                    dp[i][j]= true;
+                
+                else if(arr[i-1]<=j)
+                    dp[i][j]= dp[i-1][j] || dp[i-1][j-arr[i-1]];
+                
+                else
+                    dp[i][j]= dp[i-1][j];
+            }
+        }
+        
+        
+        int ans= INT_MAX;
+        for(int j=0; j<=sum/2; j++)
+        {
+            if(dp[size][j]==true)
+                ans= min(ans,abs(sum-2*j));
+        }
+        
+        return ans;
     }
 };
